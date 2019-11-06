881441141e320e0f793bcf994c05bf106bc83d51
wangxb <sc30logo@gmail.com>
2019-11-06 23:05:01 +0800

    User界面修改

---
+++ src/components/common/EditButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="edit-btn-con">
+        <button class="edit-btn" @click="click()">
+            <span>编辑个人资料</span>
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "EditButton",
+        methods:{
+            click() {
+                this.$emit('edit')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .edit-btn {
+        cursor: pointer;
+        padding: 7px 16px;
+        border-radius: 3px;
+        border: 1px solid #0084ff;
+        color: #0084ff;
+    }
+    .edit-btn:hover {
+        background-color: rgba(0,132,255,0.1);
+    }
+
+</style>
+<style src="../../assets/css/common.css"></style>
--- src/pages/User.vue
+++ src/pages/User.vue
@@ -1,12 +1,12 @@
 <template>
     <div id="user-page">
        <div class="user-center-pannel">
-           <div class="user-card">
+           <div class="user-card bs">
                <div class="background"></div>
                <div class="user-avatar"></div>
                <div class="user-info">
                    <div class="user-name">
-                       <span>你的名字</span>
+                       <span>{{$store.getters.profile.nickname}}</span>
                    </div>
                    <div class="user-intro">
                        <span>主人很烂没有介绍</span>
@@ -16,8 +16,15 @@
            </div>
            <div class="user-main-pannel">
                <div class="user-main-left">
-                   <div class="user-activity"></div>
-                   <div class="user-activity"></div>
+                   <div class="user-activity-filter bs">
+                       <div class="user-filter-item">我的动态</div>
+                       <div class="user-filter-item">我的点赞</div>
+                       <div class="user-filter-item">我的评论</div>
+                       <div class="user-filter-item">我的关注</div>
+                       <div class="user-filter-item">关注我的</div>
+                   </div>
+                   <moment-card class="user-moment-card bs">
+                   </moment-card>
                </div>
                <div class="user-main-right">
                    <div class="user-info-item"></div>
@@ -30,11 +37,31 @@
 </template>
 
 <script>
-    import XButton from '../components/common/Button'
+    import XButton from '../components/common/EditButton'
+    import MomentCard from '../components/MomentCard'
+    import getRequest from "../utils/api"
+
     export default {
         name: "User",
         components:{
-            XButton
+            XButton,
+            MomentCard
+        },
+        data() {
+            return {
+                momentList:[],
+                api:{
+                    userMoment:''
+                }
+            }
+        },
+        mounted() {
+            this.profileInit()
+        },
+        methods:{
+            profileInit() {
+                getRequest()
+            }
         }
     }
 </script>
@@ -53,8 +80,8 @@
     .user-card {
         width: 100%;
         height: 300px;
-        border-radius: 2px;
-        background-color: #101216;
+        border-radius: 4px;
+        background-color: #fff;
         position: relative;
     }
 
@@ -65,26 +92,26 @@
     }
 
     .user-main-left {
-        width: 550px;
+        width: 690px;
 
     }
 
     .user-main-right {
-        width: 340px;
+        width: 300px;
         margin: 0 0 0 10px;
     }
 
     .user-info-item {
         width: 100%;
-        height: 180px;
+        height: 150px;
         margin-bottom: 10px;
         border-radius: 4px;
         background-color: #FF595E;
     }
 
-    .user-activity {
-        width: 550px;
-        height: 400px;
+    .user-activity-item {
+        width: 100%;
+        height: 200px;
         margin-bottom: 10px;
         border-radius: 4px;
         background-color: #0086b3;
@@ -98,7 +125,11 @@
         vertical-align: top;
         border: 4px solid #fff;
         border-radius: 8px;
-        background-color: #a71d5d;
+        background-color: #fff;
+        background-image: url("../assets/images/jay.png");
+        background-repeat: no-repeat;
+        /*background-size: contain;*/
+        background-size: cover;
         position: absolute;
         bottom: 25px;
         left: 25px;
@@ -110,13 +141,13 @@
         height: 130px;
         background-color: #646464;
         /*background-image: url("../assets/images/cat.jpg");*/
+        /*background-position-y:180px;*/
     }
 
     .user-info {
         display: inline-block;
         width: 670px;
         height: 140px;
-        background-color: #FF595E;
         position: absolute;
         bottom: 25px;
         left: 210px;
@@ -136,6 +167,33 @@
         position: absolute;
         right: 0;
         bottom: 0;
+    }
+
+    .user-activity-filter {
+        width: 100%;
+        height: 50px;
+        margin-bottom: 10px;
+        background-color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .user-moment-card {
+        margin-top: 10px;
+        /*height: 160px;*/
+        width: 632px;
+        position: relative; /* 使用 相对定位的原因是,子元素more-panel必须使用绝对定位，见 https://www.iteye.com/blog/lixh1986-1948337,或者google搜索:css absolute 如何相对父元素*/
+        background-color: #fff;
+        border-radius: 4px;
+        font-size: 30px;
+    }
+
+    .user-filter-item {
+        /*display: inline-block;*/
+        padding: 0 35px;
+        cursor: pointer;
+
     }
 </style>
 <style src="../assets/css/common.css"></style>
